c89bbb579b5d1b101165c02b4fcff11ce76d065d
hbblo <hbblo@LAPTOP-UJKTBKSO>
2023-02-23 17:02:06 +0100

    Add project files.

---
+++ Warehouse/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Warehouse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Cześć jestem Henryk");
+        }
+    }
+}
